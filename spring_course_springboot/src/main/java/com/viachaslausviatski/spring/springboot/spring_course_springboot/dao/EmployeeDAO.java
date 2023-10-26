ffeddd08a6f1a1d05bd52cbf0b86f6af0c00453c
Viachaslau <12170068@study.bsuir.by>
2023-10-27 00:44:04 +0300

    added a springboot part

---
+++ spring_course_springboot/src/main/java/com/viachaslausviatski/spring/springboot/spring_course_springboot/dao/EmployeeDAO.java
@@ -0,0 +1,18 @@
+package com.viachaslausviatski.spring.springboot.spring_course_springboot.dao;
+
+
+
+import com.viachaslausviatski.spring.springboot.spring_course_springboot.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployees();
+
+//    public void saveEmployee(Employee employee);
+//
+//    public Employee getEmployee(int id);
+//
+//    public void deleteEmployee(int id);
+}
---
+++ spring_course_springboot/src/main/java/com/viachaslausviatski/spring/springboot/spring_course_springboot/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.viachaslausviatski.spring.springboot.spring_course_springboot.service;
+
+
+
+import com.viachaslausviatski.spring.springboot.spring_course_springboot.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+//    public void saveEmployee(Employee employee);
+//
+//    public Employee getEmployee(int id);
+//
+//    public void deleteEmployee(int id);
+}
